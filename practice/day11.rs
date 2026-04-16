bcd2b4905921ecd17fe1832d3b914f76b1291df7
Ritesh Sonawane <rdssonawane2317@gmail.com>
2026-04-16 09:11:44 +0530

    Rust Programming Day 12

--- practice/day11.rs
+++ practice/day11.rs
@@ -9,18 +9,5 @@ fn main()
 
     /* Not Valid */
     //favorite_city.push_str("Japan");
-     
-
-    let mut number = [10, 15, 17, 23, 92, 93];
-    let array_slice = &mut number[..3];
-
-    array_slice[0] = 23;
-
-    print_array(array_slice);
-    print_array(&number);
-
 }
 
-fn print_array(array:&[i32]) {
-    println!("{:?}", array.len());
-}
